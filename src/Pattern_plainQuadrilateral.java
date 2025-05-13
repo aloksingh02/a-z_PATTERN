0e6f5cf470406f155c5183435d7de6da933e98c9
Asus <singhalokkumar427@gmail.com>
2025-05-13 08:38:21 +0530

    plain quadrilateral pattern

---
+++ src/Pattern_plainQuadrilateral.java
@@ -0,0 +1,22 @@
+//sample output of plain quadrilateral
+//****
+//****
+//****
+
+import java.util.Scanner;
+public class Pattern_plainQuadrilateral {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the no. of rows");
+        int a = sc.nextInt();
+        System.out.println("Enter the no. of cols");
+        int b = sc.nextInt();
+
+        for(int i=1; i<=a; i++){   // this row
+            for(int j=1; j<=b; j++){  // this cols
+                System.out.print("*");
+            }
+            System.out.println(); //this works as to print next line
+        }
+    }
+}
